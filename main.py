39e963bbe9742e84cf1a7dbe12a8782c0b6ad4e2
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-02 18:35:48 +0530

    Added graphs

--- main.py
+++ main.py
@@ -4,16 +4,15 @@
 @Author:      sgx team
 @Time:        01/10/2021 23:32
 """
-import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+from deeplab.graph_viz import get_graphs
+from deeplab.model import DeeplabV3Plus, CompileModel
 from deeplab.params import IMAGE_SIZE, NUM_CLASSES
 from deeplab.train import train
-from deeplab.model import DeeplabV3Plus, CompileModel
 
 if __name__ == '__main__':
     deeplab_model = DeeplabV3Plus(image_size=IMAGE_SIZE, num_classes=NUM_CLASSES)
     deeplab_model = CompileModel(deeplab_model)
     print(deeplab_model.summary())
-    train(deeplab_model)
+    history = train(deeplab_model)
+    get_graphs(history)
---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+python3 main.py
