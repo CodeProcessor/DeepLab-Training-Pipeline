0c623a5f6132a8941a312977d5e22b9084b67c78
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-02 23:10:09 +0530

    Added docker shared output + dataset

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,6 @@
 IMAGE_NAME=deeplab:v2.0
 
 docker run --gpus "all" -it --rm -p 80:80 \
--v /home/dulanj/Datasets/CIHP/instance-level_human_parsing/instance-level_human_parsing:/home/shared/data:ro \
--v $(pwd)/docker_output:/home/shared/output \
+-v /home/dulanj/Datasets/CIHP/instance-level_human_parsing/instance-level_human_parsing:/home/dataset:ro \
+-v $(pwd)/output:/home/output \
 $IMAGE_NAME
