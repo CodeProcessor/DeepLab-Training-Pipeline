444b153896702df6bd3401efa68921dece02a7a7
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-02 18:36:24 +0530

    Return history

--- deeplab/train.py
+++ deeplab/train.py
@@ -8,14 +8,16 @@
 import os
 
 from deeplab.dataset import load_dataset
-from deeplab.params import EPOCHS, CKPT_DIR, TENSORBOARD_DIR
+from deeplab.params import EPOCHS, CKPT_DIR, TENSORBOARD_DIR, VAL_FREQ
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 
 
 def create_callbacks():
     lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-8)
-    ckpt_callback = ModelCheckpoint(filepath=os.path.join(CKPT_DIR, 'depplabV3plus_{epoch:02d}-{loss:.2f}.h5'),
-                                    monitor='val_loss', mode='min', save_best_only=True)
+    ckpt_callback = ModelCheckpoint(
+        filepath=os.path.join(CKPT_DIR, 'depplabV3plus_epoch-{epoch:02d}_val-loss-{val_loss:.2f}.h5'),
+        monitor='val_loss', mode='min', save_best_only=True
+    )
     tb_callback = TensorBoard(log_dir=TENSORBOARD_DIR),
     return [lr_callback, ckpt_callback, tb_callback]
 
@@ -25,12 +27,14 @@ def train(deeplab_model):
     train_data_gen, val_data_gen = load_dataset()
     # Creating callbacks
     callbacks = create_callbacks()
-
-    deeplab_model.fit(
+    # Train the model
+    history = deeplab_model.fit(
         train_data_gen,
         validation_data=val_data_gen,
         epochs=EPOCHS,
         verbose=1,
-        validation_freq=10,
+        validation_freq=VAL_FREQ,
         callbacks=callbacks
     )
+    # Return the training information
+    return history
