6ec7423722da59e08d26b1c574facf560d797827
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-02 23:06:32 +0530

    Download pretrained weights if not available - [docker build.sh]

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
 IMAGE_NAME=deeplab:v2.0
-#docker rmi $IMAGE_NAME
+
+# Download model if not available
+RESNET_TOP=models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5
+if [ -f "$RESNET_TOP" ]; then
+    echo "$RESNET_TOP exists."
+else
+    echo "$RESNET_TOP does not exist. Downloading..."
+    (
+    cd models || exit
+    bash download_models.sh
+    )
+fi
+
+# Building the docker image
 docker build -t $IMAGE_NAME .
