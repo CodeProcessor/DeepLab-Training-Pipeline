d626077820f1dd127c51209591002e29a9c257d3
Lahiru Jayakody <lahiiru@gmail.com>
2021-10-06 08:09:31 +0530

    changes for prod deployment

---
+++ deeplab/custom_metrics.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+
+class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
+    def __init__(self,
+                 y_true=None,
+                 y_pred=None,
+                 num_classes=None,
+                 name=None,
+                 dtype=None):
+        super(UpdatedMeanIoU, self).__init__(num_classes=num_classes, name=name, dtype=dtype)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.math.argmax(y_pred, axis=-1)
+        return super().update_state(y_true, y_pred, sample_weight)
