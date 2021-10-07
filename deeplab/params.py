0c7863a686b62bbda8923d2c32ef416d9762ca82
Lahiru Jayakody <lahiiru@gmail.com>
2021-10-08 00:37:15 +0530

    path changes

--- deeplab/params.py
+++ deeplab/params.py
@@ -20,8 +20,6 @@ LOAD_MODEL = False
 # Pascal images
 NUM_TRAIN_IMAGES = 5000 if PROD_SYS else 50
 NUM_VAL_IMAGES = 5000 if PROD_SYS else 10
-train_txt_file_voc = "dataset_voc/ImageSets/Segmentation/train.txt"
-val_txt_file_voc = "dataset_voc/ImageSets/Segmentation/val.txt"
 
 # CIHP images
 # NUM_TRAIN_IMAGES = -1 if PROD_SYS else 1000
@@ -32,3 +30,6 @@ CKPT_DIR = "./output/ckpt"
 TENSORBOARD_DIR = "./output/logs"
 PRED_OUTPUT = "./output/pred"
 LOAD_MODEL_FILE = 'output/ckpt/depplabV3plus_epoch-10_val-loss-2.91.h5'
+
+train_txt_file_voc = DATASET_DIR + "/ImageSets/Segmentation/train.txt"
+val_txt_file_voc = DATASET_DIR + "/ImageSets/Segmentation/val.txt"
