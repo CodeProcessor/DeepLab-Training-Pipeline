2252015811b8e33688f0e6698d62fed9cbc7060a
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-07 19:32:20 +0530

    Pascal dataset - Loading data from text files

--- deeplab/params.py
+++ deeplab/params.py
@@ -9,7 +9,7 @@ PROD_SYS = False
 
 LEARNING_RATE = 1e-5
 IMAGE_SIZE = (512, 512)
-BATCH_SIZE = 4
+BATCH_SIZE = 16 if PROD_SYS else 4
 NUM_CLASSES = 21  # use 21 for pascal voc else 20
 
 WEIGHT_DECAY = 0
@@ -18,8 +18,10 @@ VAL_FREQ = 1
 LOAD_MODEL = False
 
 # Pascal images
-NUM_TRAIN_IMAGES = 2200 if PROD_SYS else 100
-NUM_VAL_IMAGES = 713 if PROD_SYS else 20
+NUM_TRAIN_IMAGES = 5000 if PROD_SYS else 50
+NUM_VAL_IMAGES = 5000 if PROD_SYS else 10
+train_txt_file_voc = "dataset_voc/ImageSets/Segmentation/train.txt"
+val_txt_file_voc = "dataset_voc/ImageSets/Segmentation/val.txt"
 
 # CIHP images
 # NUM_TRAIN_IMAGES = -1 if PROD_SYS else 1000
