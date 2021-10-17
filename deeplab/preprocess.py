39132e7afb8d4adeac2016a1808150a0e79b4a90
Tehara-Fonseka <thanuritehara@gmail.com.com>
2021-10-17 13:00:57 +0530

    commented out the previous image and label padding

--- deeplab/preprocess.py
+++ deeplab/preprocess.py
@@ -15,26 +15,26 @@ class PreProcess(tf.keras.layers.Layer):
         self.resize = tf.keras.layers.Resizing(height=self.target_height, width=self.target_width)
 
     def call(self, image, label):
-        rescaled_size = tf.shape(label)[:2]
-        min_index = tf.math.argmin(rescaled_size)
-        pad_value = abs(rescaled_size[0] - rescaled_size[1])
-
-        vertical_pad_up = 0
-        vertical_pad_down = 0
-        horizontal_pad_right = 0
-        horizontal_pad_left = 0
-
-        if min_index == 0:
-            vertical_pad_up = pad_value // 2
-            vertical_pad_down = pad_value - vertical_pad_up
-        else:
-            horizontal_pad_left = pad_value // 2
-            horizontal_pad_right = pad_value - horizontal_pad_left
-
-        padding = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
-        # padding_label = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
-        image = tf.pad(image, padding, "CONSTANT", constant_values=0)
-        label = tf.pad(label, padding, "CONSTANT", constant_values=0)
+        # rescaled_size = tf.shape(label)[:2]
+        # min_index = tf.math.argmin(rescaled_size)
+        # pad_value = abs(rescaled_size[0] - rescaled_size[1])
+        #
+        # vertical_pad_up = 0
+        # vertical_pad_down = 0
+        # horizontal_pad_right = 0
+        # horizontal_pad_left = 0
+        #
+        # if min_index == 0:
+        #     vertical_pad_up = pad_value // 2
+        #     vertical_pad_down = pad_value - vertical_pad_up
+        # else:
+        #     horizontal_pad_left = pad_value // 2
+        #     horizontal_pad_right = pad_value - horizontal_pad_left
+        #
+        # padding = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
+        # # padding_label = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
+        # image = tf.pad(image, padding, "CONSTANT", constant_values=0)
+        # label = tf.pad(label, padding, "CONSTANT", constant_values=0)
 
         image = self.resize(image)
         label = self.resize(label)
