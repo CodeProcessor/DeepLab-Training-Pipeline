9ebc537bd5709e31abb70fe81015c2a73d322176
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-10-19 23:49:02 +0530

    Do intensity augmentations before preprocessing Removed augmentations using opencv

--- deeplab/preprocess.py
+++ deeplab/preprocess.py
@@ -4,9 +4,12 @@
 @Author:      dulanj
 @Time:        07/10/2021 20:49
 """
-import tensorflow as tf
+import random
 
-from deeplab.params import IMAGE_SIZE, IGNORED_CLASS_ID
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+from deeplab.params import IMAGE_SIZE, IGNORED_CLASS_ID, AUG_PROBABILITY
 
 
 class PreProcess(tf.keras.layers.Layer):
@@ -21,10 +24,22 @@ class PreProcess(tf.keras.layers.Layer):
         self.rgb_mean = tf.math.multiply(tf.ones((IMAGE_SIZE[0], IMAGE_SIZE[1], 3)),
                                          tf.constant(PreProcess._MEAN_RGB, dtype=tf.float32))
 
+        self.hue = (lambda x: tf.image.random_hue(x, 0.1), AUG_PROBABILITY["hue"])
+        self.gau2d = (
+            lambda x: tfa.image.gaussian_filter2d(x, filter_shape=(3, 3), sigma=1.0), AUG_PROBABILITY["gaussian"])
+
     def call(self, image, label):
         mask = label != 255
         label = tf.where(mask, x=label, y=IGNORED_CLASS_ID)
         image = self.resize(image)
+
+        """
+        Image(inputs) only augmentations (pixel-wise)
+        """
+        for _aug, _prob in [self.hue, self.gau2d]:
+            if random.random() < _prob:
+                image = _aug(image)
+
         # image = tf.subtract(image, self.rgb_mean)
         if self.backbone == "resnet50":
             image = tf.keras.applications.resnet50.preprocess_input(image)
--- deeplab/utils.py
+++ deeplab/utils.py
@@ -4,7 +4,7 @@
 @Author:      https://github.com/leimao/DeepLab-V3, teharaf
 @Time:        17/10/2021 19:22
 """
-from random import random
+import random
 
 import cv2
 import numpy as np
@@ -57,6 +57,48 @@ def random_crop(image, label, output_size):
     return image_cropped, label_cropped
 
 
+def intensity_aug(image):
+    # This is not used
+
+    # Blur
+    if np.random.random() < AUG_PROBABILITY["gaussian"]:
+        image = cv2.blur(src=image, ksize=(3, 3))
+
+    # Brightness
+    def brightness(img, val):
+        value = random.random() * val
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        hsv = np.array(hsv, dtype=np.float64)
+        # hsv[:, :, 1] = hsv[:, :, 1] + value
+        # hsv[:, :, 1][hsv[:, :, 1] > 127] = 127
+        hsv[:, :, 2] = hsv[:, :, 2] + value
+        hsv[:, :, 2][hsv[:, :, 2] > 127] = 127
+        hsv = np.array(hsv, dtype=np.uint8)
+        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+        return img
+
+    if np.random.random() < AUG_PROBABILITY["brightness"]:
+        image = brightness(image, 30)
+
+    # Hue
+    def hue(img, val):
+        value = random.random() * val
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        hsv = np.array(hsv, dtype=np.float64)
+        _c = 2
+        hsv[:, :, _c] = hsv[:, :, _c] + value
+        _cond = hsv[:, :, 1] > 127
+        hsv[:, :, _c][_cond] = hsv[:, :, _c][_cond] % 127
+        hsv = np.array(hsv, dtype=np.uint8)
+        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+        return img
+
+    if np.random.random() < AUG_PROBABILITY["hue"]:
+        image = hue(image, 1)
+
+    return image
+
+
 def augment(image, label, output_size=IMAGE_SIZE, min_scale_factor=0.5, max_scale_factor=2.0):
     original_height = image.shape[0]
     original_width = image.shape[1]
@@ -90,41 +132,6 @@ def augment(image, label, output_size=IMAGE_SIZE, min_scale_factor=0.5, max_scal
     # Flip image and label
     if np.random.random() < AUG_PROBABILITY["flip"]:
         image, label = flip_image_and_label(image=image, label=label)
-
-    # Blur
-    if np.random.random() < AUG_PROBABILITY["gaussian"]:
-        image = cv2.blur(src=image, ksize=(3, 3))
-
-    # Brightness
-    def brightness(img, low, high):
-        value = random.uniform(low, high)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hsv = np.array(hsv, dtype=np.float64)
-        hsv[:, :, 1] = hsv[:, :, 1] * value
-        hsv[:, :, 1][hsv[:, :, 1] > 255] = 255
-        hsv[:, :, 2] = hsv[:, :, 2] * value
-        hsv[:, :, 2][hsv[:, :, 2] > 255] = 255
-        hsv = np.array(hsv, dtype=np.uint8)
-        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        return img
-
-    if np.random.random() < AUG_PROBABILITY["brightness"]:
-        image = brightness(image, 0.5, 1.5)
-
-    # Hue
-    def hue(img, val):
-        value = random.random() * val
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hsv = np.array(hsv, dtype=np.float64)
-        hsv[:, :, 0] = hsv[:, :, 0] + value
-        _cond = hsv[:, :, 1] > 255
-        hsv[:, :, 0][_cond] = hsv[:, :, 0][_cond] % 255
-        hsv = np.array(hsv, dtype=np.uint8)
-        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        return img
-
-    if np.random.random() < AUG_PROBABILITY["hue"]:
-        image = hue(image, 20)
 
     label = np.expand_dims(label, axis=2)
     return image, label
