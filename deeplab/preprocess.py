88e2c5b2669f50ecabffa5b46380c075c0325691
Lahiru Jayakody <lahiiru@gmail.com>
2021-10-19 18:21:14 +0530

    merge issue fix

--- deeplab/preprocess.py
+++ deeplab/preprocess.py
@@ -21,28 +21,28 @@ class PreProcess(tf.keras.layers.Layer):
         self.rgb_mean = tf.math.multiply(tf.ones((IMAGE_SIZE[0], IMAGE_SIZE[1], 3)), tf.constant(PreProcess._MEAN_RGB, dtype=tf.float32))
 
     def call(self, image, label):
-        rescaled_size = tf.shape(label)[:2]
-        mask = label != 255
-        label = tf.where(mask, x=label, y=IGNORED_CLASS_ID)
-        min_index = tf.math.argmin(rescaled_size)
-        pad_value = abs(rescaled_size[0] - rescaled_size[1])
-
-        vertical_pad_up = 0
-        vertical_pad_down = 0
-        horizontal_pad_right = 0
-        horizontal_pad_left = 0
-
-        if min_index == 0:
-            vertical_pad_up = pad_value // 2
-            vertical_pad_down = pad_value - vertical_pad_up
-        else:
-            horizontal_pad_left = pad_value // 2
-            horizontal_pad_right = pad_value - horizontal_pad_left
-
-        padding = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
-        # padding_label = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
-        image = tf.pad(image, padding, "CONSTANT", constant_values=0)
-        label = tf.pad(label, padding, "CONSTANT", constant_values=0)
+        # rescaled_size = tf.shape(label)[:2]
+        # mask = label != 255
+        # label = tf.where(mask, x=label, y=IGNORED_CLASS_ID)
+        # min_index = tf.math.argmin(rescaled_size)
+        # pad_value = abs(rescaled_size[0] - rescaled_size[1])
+        #
+        # vertical_pad_up = 0
+        # vertical_pad_down = 0
+        # horizontal_pad_right = 0
+        # horizontal_pad_left = 0
+        #
+        # if min_index == 0:
+        #     vertical_pad_up = pad_value // 2
+        #     vertical_pad_down = pad_value - vertical_pad_up
+        # else:
+        #     horizontal_pad_left = pad_value // 2
+        #     horizontal_pad_right = pad_value - horizontal_pad_left
+        #
+        # padding = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
+        # # padding_label = [[vertical_pad_up, vertical_pad_down], [horizontal_pad_left, horizontal_pad_right], [0, 0]]
+        # image = tf.pad(image, padding, "CONSTANT", constant_values=0)
+        # label = tf.pad(label, padding, "CONSTANT", constant_values=0)
 
         image = self.resize(image)
         image = tf.subtract(image, self.rgb_mean)
